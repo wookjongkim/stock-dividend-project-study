fb172763d25c73a8f999f562b117c0ca64a0a02d
wookjongkim <121083077+wookjongkim@users.noreply.github.com>
2023-05-24 09:31:09 +0900

    first commit

---
+++ src/main/java/zerobase/stockdividendprojectstudy/StockDividendProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package zerobase.stockdividendprojectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendProjectStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDividendProjectStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/stockdividendprojectstudy/StockDividendProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.stockdividendprojectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDividendProjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
